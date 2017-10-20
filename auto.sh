5d7e5db25e399ebdfe4b68e96e81a0dd63ecf9cf
Your Name <you@example.com>
2017-10-20 12:19:51 +0800

    bce branch

--- auto.sh
+++ auto.sh
@@ -2,12 +2,12 @@
 pwd
 git pull origin master
 git push origin master
-ssh ubuntu@aws3.yongbuzhixi.com<< EOF
-cd ~/docker-nginx-https
+ssh root@bce.yongbuzhixi.com<< EOF
+cd ~/docker/docker-nginx-https
 git log -1
 git pull origin master
+git checkout bce
 git log -1
-date
 git status
 ls ./config/nginx/certs/
 chmod +x run.sh
